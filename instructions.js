a5b2344ae335d460f7c81c5c4681dbddc0f9fcc2
Cameron Smith <smithcg2@appstate.edu>
2017-11-03 18:32:24 -0400

    WIP

---
+++ instructions.js
@@ -0,0 +1,35 @@
+var instructions = {
+    a1: "";
+    a2: "";
+    a3: "";
+    a4: "";
+    a5: "";
+    a6: "";
+    a7: "";
+    a8: "";
+    a9: "";
+    b1: "";
+    b2: "";
+    b3: "";
+    b4: "";
+    c1: "";
+    d1: "";
+}
+
+var section = {
+    a1: "Schedule A: Group (1)\nMACHINERY & EQUIPMENT";
+    a2: "Schedule A: Group (2)\nOFFICE FURNITURE AND FIXTURES";
+    a3: "Schedule A: Group (3)\nCOMPUTER EQUIPMENT";
+    a4: "Schedule A: Group (4)\nLEASEHOLD IMPROVEMENTS";
+    a5: "Schedule A: Group (5)\nRENTAL EQUIPMENT";
+    a6: "Schedule A: Group (6)\nHEAVY EQUIPMENT";
+    a7: "Schedule A: Group (7)\nOTHER EQUIPMENT AND FARM EQUIPMENT";
+    a8: "Schedule A: Group (8)\nSIGNS AND BILLBOARDS";
+    a9: "Schedule A: Group (9)\nMISC";
+    b1: "Schedule B: Group (1)\nUNREGISTERED MOTOR VEHICLES AND MULTIYEAR TAGGED TRAILERS";
+    b2: "Schedule B: Group (2)\nSPECIAL BODIES AND EQUIPMENT ATTACHMENTS TO REGISTERED MOTOR VEHICLES";
+    b3: "";
+    b4: "";
+    c1: "";
+    d1: "";
+}
