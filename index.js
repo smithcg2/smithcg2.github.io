a60f2d7e3e1acde49461bbcd60c1955d81094a68
Samantha <ollersi@appstate.edu>
2017-11-28 18:33:54 -0500

    update form

--- index.js
+++ index.js
@@ -9,14 +9,43 @@ function buildinfo(){
 
 function buildtable() {
     var container = $("#businfo");
-    var table = $("<table>");
+    var table = $("<table>", {id: "bustable"});
     for (var i = 0; i < labels.length; i++)
     {
         var row = $("<tr>");
-
-        var input = $("<input>", {id: ids[i]});
+        var cell1 = $("<td>");
+        cell1.text(labels[i]);
+        cell1.appendTo(row);
+        var cell2 = $("<td>");
+        
+        var input;
+        if (ids[i] == "bustype")
+        {
+            input = $("<select>", {id: ids[i]});
+            $.each(types, function(i, option) {
+                input.append("<option>" + option + "</option>");
+            });
+        }
+        else if (ids[i] == "buscat")
+        {
+            input = $("<select>", {id: ids[i]});
+            $.each(categories, function(i, option) {
+                input.append("<option>" + option + "</option>");
+            });
+        }
+        else
+        {
+            input = $("<input>", {id: ids[i]});
+        }
+        input.appendTo(cell2);
+        cell2.appendTo(row);
+        row.appendTo(table);
     }
 
+    table.appendTo(container);
+    var button = $("<button>", {id: "SaveInfo", class: "savebutton"});
+    button.text("Save Information");
+    button.appendTo(container);
 }
 
 function init() {
