68452234a9c1d37954a5bc024bb95eb03db7184d
Cameron Smith <smithcg2@appstate.edu>
2017-11-28 18:03:48 -0500

    a

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
+var labels = ["Company Information", "Business Category", "Business Type", "Year Of Filing", "Address"];
+var ids = ["compinfo", "buscat", "bustype", "fileyear", "address"];
+var categories = ["Retail", "Wholesale", "Manufacturing", "Service", "Leasing/Rental", "Farming", "Other"];
+var types = ["Corporation", "Sole Proprietorship", "Partnership", "Unincorporated Association", "Other"];
 
 function buildinfo(){
 
 }
 
 function buildtable() {
+    var container = $("#businfo");
+    var table = $("<table>");
+    for (var i = 0; i < labels.length; i++)
+    {
+        var row = $("<tr>");
+
+        var input = $("<input>", {id: ids[i]});
+    }
 
 }
+
+function init() {
+    buildtable();
+}
+
+window.onload = init;
